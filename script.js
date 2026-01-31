a08d6981a6f6288340509677ba280cdb540ad07d
KoryakinaAnastasiia <koryakinaanastasiia@gmail.com>
2026-01-31 22:12:20 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("mobileMenu");
+const btnclose = document.getElementById("menuBtnClose");
 
+btn.addEventListener("click", () => {
+  // Переключаем класс 'active' у меню
+  menu.classList.toggle("active");
+});
+
+// Закрытие меню при клике вне его области
+btnclose.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
