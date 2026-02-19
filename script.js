1f210d4b8bdaccada2283f431ec3fc0f3a563472
KoryakinaAnastasiia <koryakinaanastasiia@gmail.com>
2026-02-19 23:57:39 +0300

    добавила флажки

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ btnclose.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
+//language
+
 const buttons = document.querySelectorAll(".header__footer_lang_button");
 
 buttons.forEach((btn) => {
@@ -21,4 +23,14 @@ buttons.forEach((btn) => {
     // Добавляем класс только нажатой
     this.classList.add("active");
   });
+});
+
+//street
+
+const street_name = document.getElementById("str");
+const point_m = document.getElementById("point_map");
+
+point_map.addEventListener("click", () => {
+  // Переключаем класс 'active' у меню
+  menu.classList.toggle("active");
 });
